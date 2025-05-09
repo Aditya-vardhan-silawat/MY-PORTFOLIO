e14384c66e49e08c8dc575c04d6e1901ae09547a
Aditya-vardhan-silawat <aditya.vardhan0409@gmail.com>
2025-05-09 13:52:53 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,14 @@
+const sections = document.querySelectorAll('section');
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.animationPlayState = 'running';
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+sections.forEach(sec => {
+  observer.observe(sec);
+});
